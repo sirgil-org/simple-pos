b176937049ca436df6fa55e4f49191cec20f22d1
Pelema <abpalema@gmail.com>
2023-11-16 12:51:56 +0200

    Switched to ionic router

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,19 +1,24 @@
-import { Switch, Route } from "wouter";
-import About from "../pages/about";
-import Orders from "../pages/orders/orders";
-import NewOrder from "../pages/new_order";
-import Reports from "../pages/reports";
-import Expenses from "../pages/expenses";
-
+import { Route, Redirect } from "react-router-dom";
+// import About from "../pages/about";
+// import Orders from "../pages/orders/orders";
+// import NewOrder from "../pages/new_order";
+// import Reports from "../pages/reports";
+// import Expenses from "../pages/expenses";
+import { IonRouterOutlet } from "@ionic/react";
+import Tabs from "./tabs";
 
 const routes = () => (
-    <Switch>
-      <Route path="/" component={Orders} />
-      <Route path="/about" component={About} />
-      <Route path="/new" component={NewOrder} />
-      <Route path="/reports" component={Reports} />
-      <Route path="/expenses" component={Expenses} />
-    </Switch>
+  <IonRouterOutlet>
+    {/* <Route exact path="/" component={Orders} /> */}
+    {/* <Route exact path="/about" component={About} />
+      <Route exact path="/new" component={NewOrder} />
+      <Route exact path="/reports" component={Reports} />
+      <Route exact path="/expenses" component={Expenses} /> */}
+    <Route path="/tabs" render={() => <Tabs />} />
+    <Route exact path="/">
+      <Redirect to="/tabs" />
+    </Route>
+  </IonRouterOutlet>
 );
 
 export default routes;
