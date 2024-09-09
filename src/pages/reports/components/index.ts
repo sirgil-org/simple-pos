0a7b5dbd1950564a3ea1e570bd39a556d93b42da
euricodande12 <euricodande12@gmail.com>
2024-09-09 12:14:19 +0200

    White text border in the login input fixed.

--- src/pages/reports/components/index.ts
+++ src/pages/reports/components/index.ts
@@ -1,4 +1,4 @@
-import CustomAreaChart from "./custom_area_chart";
+//import CustomAreaChart from "./custom_area_chart";
 import ReportSkeletal from "./report_skeletal";
 
-export { ReportSkeletal, CustomAreaChart };
+export { ReportSkeletal};
