eb47c1b08ebf6f0c863a8020d5ce810ea2ea8a9c
Pelema <abpalema@gmail.com>
2024-10-14 17:45:18 +0200

    Added back button forgot password

--- src/pages/reset-password/index.tsx
+++ src/pages/reset-password/index.tsx
@@ -1,10 +1,28 @@
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
 export default function ResetPasswordPage() {
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/login">Back</IonBackButton>
+          </IonButtons>
+          <IonTitle>Reset Password</IonTitle>
+        </IonToolbar>
+      </IonHeader>
       <IonContent className="ion-padding">
-        <div style={{ paddingTop: "env(safe-area-inset-top)" }}>Reset password</div>
+        <div style={{ paddingTop: "env(safe-area-inset-top)" }}>
+          Reset password
+        </div>
       </IonContent>
     </IonPage>
   );
